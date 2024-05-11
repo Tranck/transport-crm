889574c460f3483117d6213a5ffefadb56587673
Tranck <achernyadvki@gmail.com>
2024-05-11 19:40:37 +0300

    AT-1: Fix queries to database

--- src/main/resources/db/changelog/changes/sql/V20240504123900__create_table_for_account.sql
+++ src/main/resources/db/changelog/changes/sql/V20240504123900__create_table_for_account.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS public.account
     username    VARCHAR(255)    UNIQUE NOT NULL,
     password    VARCHAR(255)    NOT NULL,
     role        BIGINT          NOT NULL,
-    CONSTRAINT fk_account_role_account_role_id FOREIGN KEY (role) REFERENCES public.account_role (id);
+    CONSTRAINT fk_account_role_account_role_id FOREIGN KEY (role) REFERENCES public.account_role (id)
 );
--- src/main/resources/db/changelog/changes/sql/V20240511183500__create_table_account_role_authority_link.sql
+++ src/main/resources/db/changelog/changes/sql/V20240511183500__create_table_account_role_authority_link.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS public.account_role_authority_link
     role        BIGINT,
     authority   BIGINT,
     CONSTRAINT fk_account_role_authority_link_role_account_role_id
-        FOREIGN KEY (role) REFERENCES public.account_role (id)
+        FOREIGN KEY (role) REFERENCES public.account_role (id),
     CONSTRAINT fk_account_role_authority_link_authority_account_role_authority_id
         FOREIGN KEY (authority) REFERENCES public.account_role_authority (id)
 );
